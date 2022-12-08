9ab09678a2eed0e860557741c31d8aba8c6d84cf
Muhammad Haggy <86955979+muhammadHaggy@users.noreply.github.com>
2022-12-08 12:58:17 +0700

    add styling to profile

--- canwe/lib/auth/profile.dart
+++ canwe/lib/auth/profile.dart
@@ -39,30 +39,52 @@ class _ProfilePageState extends State<ProfilePage> {
             return const Center(child: CircularProgressIndicator());
           } else {
             var profile = snapshot.data;
-            return Form(
-              key: _formKey,
-              child: LayoutBuilder(
-                builder: (p0, p1) {
-                  return SingleChildScrollView(
-                    child: ConstrainedBox(
-                      constraints: BoxConstraints(minHeight: p1.maxHeight),
-                      child: Container(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Column(
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: TextFormField(
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30.0),
+              child: Form(
+                key: _formKey,
+                child: LayoutBuilder(
+                  builder: (p0, p1) {
+                    return SingleChildScrollView(
+                      child: ConstrainedBox(
+                        constraints: BoxConstraints(minHeight: p1.maxHeight),
+                        child: Container(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                children: [
+                                  Center(
+                                    child: Padding(
+                                      padding: EdgeInsets.only(bottom: 20),
+                                      child: Text(
+                                        'Edit Profile',
+                                        style: TextStyle(
+                                          fontSize: 30,
+                                          fontWeight: FontWeight.w700,
+                                          color:
+                                              Color.fromRGBO(64, 105, 225, 1),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: double.infinity,
+                                    child: Text(
+                                      "Username",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 16),
+                                    ),
+                                  ),
+                                  const SizedBox(height: 8),
+                                  TextFormField(
                                     initialValue: profile!.username,
                                     decoration: InputDecoration(
-                                        labelText: "Username",
                                         border: OutlineInputBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(5.0),
-                                        )),
+                                      borderRadius: BorderRadius.circular(12.0),
+                                    )),
                                     onChanged: (String? value) {
                                       setState(() {
                                         profile.username = value!;
@@ -82,17 +104,23 @@ class _ProfilePageState extends State<ProfilePage> {
                                       return null;
                                     },
                                   ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: TextFormField(
+                                  const SizedBox(height: 24),
+                                  SizedBox(
+                                    width: double.infinity,
+                                    child: Text(
+                                      "First Name",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 16),
+                                    ),
+                                  ),
+                                  const SizedBox(height: 8),
+                                  TextFormField(
                                     initialValue: profile.firstName,
                                     decoration: InputDecoration(
-                                        labelText: "first name",
                                         border: OutlineInputBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(5.0),
-                                        )),
+                                      borderRadius: BorderRadius.circular(12.0),
+                                    )),
                                     onChanged: (String? value) {
                                       setState(() {
                                         profile.firstName = value!;
@@ -105,17 +133,23 @@ class _ProfilePageState extends State<ProfilePage> {
                                       });
                                     },
                                   ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: TextFormField(
+                                  const SizedBox(height: 24),
+                                  SizedBox(
+                                    width: double.infinity,
+                                    child: Text(
+                                      "Last Name",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 16),
+                                    ),
+                                  ),
+                                  const SizedBox(height: 8),
+                                  TextFormField(
                                     initialValue: profile.lastName,
                                     decoration: InputDecoration(
-                                        labelText: "last name",
                                         border: OutlineInputBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(5.0),
-                                        )),
+                                      borderRadius: BorderRadius.circular(12.0),
+                                    )),
                                     onChanged: (String? value) {
                                       setState(() {
                                         profile.lastName = value!;
@@ -128,17 +162,23 @@ class _ProfilePageState extends State<ProfilePage> {
                                       });
                                     },
                                   ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: TextFormField(
+                                  const SizedBox(height: 24),
+                                  SizedBox(
+                                    width: double.infinity,
+                                    child: Text(
+                                      "Email Address",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 16),
+                                    ),
+                                  ),
+                                  const SizedBox(height: 8),
+                                  TextFormField(
                                     initialValue: profile.email,
                                     decoration: InputDecoration(
-                                        labelText: "Email address",
                                         border: OutlineInputBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(5.0),
-                                        )),
+                                      borderRadius: BorderRadius.circular(12.0),
+                                    )),
                                     onChanged: (String? value) {
                                       setState(() {
                                         profile.email = value!;
@@ -151,11 +191,17 @@ class _ProfilePageState extends State<ProfilePage> {
                                       });
                                     },
                                   ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: InputDatePickerFormField(
-                                    fieldLabelText: "Tanggal Lahir",
+                                  const SizedBox(height: 24),
+                                  SizedBox(
+                                    width: double.infinity,
+                                    child: Text(
+                                      "Tanggal Lahir",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 16),
+                                    ),
+                                  ),
+                                  InputDatePickerFormField(
                                     initialDate: profile.tanggalLahir,
 
                                     // Menambahkan behavior saat data disimpan
@@ -168,17 +214,23 @@ class _ProfilePageState extends State<ProfilePage> {
                                         DateTime.fromMicrosecondsSinceEpoch(0),
                                     lastDate: DateTime.now(),
                                   ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: TextFormField(
+                                  const SizedBox(height: 24),
+                                  SizedBox(
+                                    width: double.infinity,
+                                    child: Text(
+                                      "Bio Singkat",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 16),
+                                    ),
+                                  ),
+                                  const SizedBox(height: 8),
+                                  TextFormField(
                                     initialValue: profile.bioSingkat,
                                     decoration: InputDecoration(
-                                        labelText: "Bio Singkat",
                                         border: OutlineInputBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(5.0),
-                                        )),
+                                      borderRadius: BorderRadius.circular(12.0),
+                                    )),
                                     onChanged: (String? value) {
                                       setState(() {
                                         profile.bioSingkat = value!;
@@ -191,54 +243,74 @@ class _ProfilePageState extends State<ProfilePage> {
                                       });
                                     },
                                   ),
-                                ),
-                              ],
-                            ),
-                            TextButton(
-                              style: ButtonStyle(
-                                backgroundColor:
-                                    MaterialStateProperty.all(Colors.blue),
+                                  const SizedBox(height: 40),
+                                ],
                               ),
-                              onPressed: () async {
-                                if (_formKey.currentState!.validate()) {
-                                  _formKey.currentState!.save();
-                                  final profileData = Profile(
-                                    status: true,
-                                    username: username,
-                                    firstName: firstName,
-                                    lastName: lastName,
-                                    email: email,
-                                    tanggalLahir: tanggalLahir,
-                                    bioSingkat: bioSingkat,
-                                  );
-                                  print('valid');
-                                  final response = await request.post(
-                                    "https://canwe.pythonanywhere.com/user/profile/json",
-                                    profileData.toJson(),
-                                  );
-                                  print(profileData.toJson());
-                                  print(response);
-                                  if (response["status"].toString() == "true") {
-                                    // Code here will run if the login succeeded.
-                                    print("Edit Profil Succesfull");
-                                    setState(() {});
-                                  } else {
-                                    // Code here will run if the login failed (wrong username/password).
-                                    print("Edit profil failed");
+                              GestureDetector(
+                                onTap: () async {
+                                  if (_formKey.currentState!.validate()) {
+                                    _formKey.currentState!.save();
+                                    final profileData = Profile(
+                                      status: true,
+                                      username: username,
+                                      firstName: firstName,
+                                      lastName: lastName,
+                                      email: email,
+                                      tanggalLahir: tanggalLahir,
+                                      bioSingkat: bioSingkat,
+                                    );
+                                    print('valid');
+                                    final response = await request.post(
+                                      "https://canwe.pythonanywhere.com/user/profile/json",
+                                      profileData.toJson(),
+                                    );
+                                    print(profileData.toJson());
+                                    print(response);
+                                    if (response["status"].toString() ==
+                                        "true") {
+                                      // Code here will run if the login succeeded.
+                                      print("Edit Profil Succesfull");
+                                      setState(() {});
+                                    } else {
+                                      // Code here will run if the login failed (wrong username/password).
+                                      print("Edit profil failed");
+                                    }
                                   }
-                                }
-                              },
-                              child: const Text(
-                                "Save Changes",
-                                style: TextStyle(color: Colors.white),
+                                },
+                                child: Container(
+                                  width: 200,
+                                  height: 45,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    color: Color.fromARGB(255, 224, 227, 231),
+                                  ),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Text(
+                                        "Save Changes",
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.w600,
+                                            color: Colors.black,
+                                            fontSize: 19),
+                                      ),
+                                      SizedBox(width: 15),
+                                      Icon(
+                                        Icons.save,
+                                        color: Colors.black,
+                                        size: 26,
+                                      )
+                                    ],
+                                  ),
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  );
-                },
+                    );
+                  },
+                ),
               ),
             );
           }
--- canwe/lib/main.dart
+++ canwe/lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.blue,
         ),
         // home: const MyHomePage(title: 'Flutter Demo Home Page'),
-        home: const RegisterPage(),
+        home: const LoginPage(),
       ),
     );
   }
